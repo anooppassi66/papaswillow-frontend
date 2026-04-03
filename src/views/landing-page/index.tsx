6aacda7ec0876f2a40b163e6414f1c725625d25f
Anoop <anooppassi66@gmail.com>
2026-04-03 17:51:45 +0530

    changes

--- src/views/landing-page/index.tsx
+++ src/views/landing-page/index.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import { store, useSelector } from 'store';
 
 /*api calls*/
@@ -6,11 +6,6 @@ import { store, useSelector } from 'store';
 
 /*material <Ui*/
 import Box from '@mui/material/Box';
-import Dialog from '@mui/material/Dialog';
-import DialogTitle from '@mui/material/DialogTitle';
-import DialogContent from '@mui/material/DialogContent';
-import DialogActions from '@mui/material/DialogActions';
-import Button from '@mui/material/Button';
 
 /*components*/
 import BannerDashBoard from 'ui-component/banner/BannerDashBoard';
@@ -28,7 +23,6 @@ import { homeProducts } from 'store/slices/productStore';
 // ==============================|| Home Page ||============================== //
 const LandingPage = () => {
     const { FeaturedProducts, DiscountProducts, HotDeals, isLoading, isSuccess } = useSelector((state) => state.store.products);
-    const [popupOpen, setPopupOpen] = useState(true);
 
     console.log('55featuredProductsfeaturedProducts', FeaturedProducts);
     //hotDealsProducts
@@ -36,24 +30,8 @@ const LandingPage = () => {
         store.dispatch(homeProducts());
     }, []);
 
-    const handleClosePopup = () => {
-        setPopupOpen(false);
-    };
-
     return (
         <>
-            <Dialog open={popupOpen} onClose={handleClosePopup}>
-                <DialogTitle sx={{ pb: 1 }}>Game Break! 🏏</DialogTitle>
-                <DialogContent>
-                    Our website is under maintenance and will be back stronger soon.
-                    Meanwhile, you can place your orders by calling <a href="tel:+1 (409) 344-3513">+1 (409) 344-3513</a>
-                </DialogContent>
-                <DialogActions>
-                    <Button onClick={handleClosePopup} color="primary" variant="contained">
-                        Close
-                    </Button>
-                </DialogActions>
-            </Dialog>
             <Box className="2345" sx={{ mb: '20px' }}>
                 <BannerDashBoard />
             </Box>
