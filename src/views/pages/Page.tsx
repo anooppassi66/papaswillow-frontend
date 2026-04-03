2419f1bd86a825f3ba8f416ec5aa2e1481b2cc4f
Anoop <anooppassi66@gmail.com>
2026-04-03 16:10:25 +0530

    changes

--- src/views/pages/Page.tsx
+++ src/views/pages/Page.tsx
@@ -1,41 +1,55 @@
 // @ts-nocheck
-import React, { forwardRef, ReactNode } from 'react';
+"use client";
+
+import React, { forwardRef, ReactNode, useEffect, useState } from 'react';
 import { Helmet } from 'react-helmet-async';
 // @mui
-import { useTheme, styled } from '@mui/material/styles';
+import { styled } from '@mui/material/styles';
 import Box from '@mui/material/Box';
 import BoxProps from '@mui/material/BoxProps';
 
 // ----------------------------------------------------------------------
 
+useEffect(() => {
+  const timer = setTimeout(() => setOpen(true), 3000);
+  return () => clearTimeout(timer);
+}, []);
+
 const StyledBox = styled(Box)(({ theme }) => ({
-    flexGrow: 1,
-    width: '100%',
-    height: '100%'
+  flexGrow: 1,
+  width: '100%',
+  height: '100%',
 }));
 
 interface Props extends BoxProps {
-    children: ReactNode;
-    title: string;
-    favicon?: null | string;
-    hideScroll?: boolean;
-    meta?: ReactNode;
+  children: ReactNode;
+  title: string;
+  favicon?: null | string;
+  hideScroll?: boolean;
+  meta?: ReactNode;
 }
 
-const Page = forwardRef<HTMLDivElement, Props>(({ children, title = '', favicon = null, hideScroll = false, meta, ...other }, ref) => {
-    return (
-        <StyledBox style={hideScroll ? { overflow: 'hidden' } : {}}>
-            {/* <Helmet>
-                <title>Arionplay</title>
-                {favicon && <link id="favicon" rel="icon" href={favicon} type="image/x-icon" />}
-                {meta}
-            </Helmet> */}
+const Page = forwardRef<HTMLDivElement, Props>(
+  ({ children, title = '', favicon = null, hideScroll = false, meta, ...other }, ref) => {
 
-            <React.Fragment ref={ref} {...other}>
-                {children}
-            </React.Fragment>
-        </StyledBox>
+    const [open, setOpen] = useState(false);
+
+    useEffect(() => {
+      setOpen(true); // 👈 popup opens on page load
+    }, []);
+
+    const handleClose = () => setOpen(false);
+
+    return (
+      <StyledBox style={hideScroll ? { overflow: 'hidden' } : {}}>
+
+        {/* Page Content */}
+        <React.Fragment ref={ref} {...other}>
+          {children}
+        </React.Fragment>
+      </StyledBox>
     );
-});
+  }
+);
 
 export default Page;
